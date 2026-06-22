c3d70e51c03d518a009e759b6350d693972f00ab
Niklas Hartung <niklas.hartung@uni-potsdam.de>
2026-06-22 13:22:34 +0200

    fix: update unit test syntax in test-to_analytical

--- tests/testthat/test-to_analytical.R
+++ tests/testthat/test-to_analytical.R
@@ -1,55 +1,91 @@
-test_that("1-CMT model can be solved analytically with / without hard-wired parameters", {
 
-    # Testing 1-CMT model with and without hard-wired parameters
-    M <- multiCompModel(ncomp = 1, type = "micro")
-    M$compartments$initial <- list(10)
+test_that("1-CMT analytical solution handles fixed and free parameters", {
+    M <- compartment_model() |>
+        add_compartment("cen", volume = NA_real_) |>
+        add_molecule("drug", cmt = "cen", initial = 10, type = "amount") |>
+        add_transport("cen", "", const = "kc0", molec = "drug")
 
     params <- list(kc0 = 1)
+    times <- 0:3
+    expected <- cbind(time = times, a_drug_cen = 10 * exp(-times))
 
-    sol1 <- to_analytical(M, paramValues = params)
-    y1 <- sol1$statefun(t = 0:3)
+    sol_free <- M |> 
+        to_analytical()
+    sol_fixed <- M |> 
+        add_parameter(param = do.call(parameters, params)) |> 
+        to_analytical()
 
-    sol2 <- to_analytical(M)
-    y2 <- sol2$statefun(t = 0:3, params)
-
-    expect_equal(y1,y2)
-
+    expect_equal(sol_fixed$statefun(times), expected)
+    expect_equal(sol_free$statefun(times, params), expected)
+    expect_equal(sol_fixed$freeParams, character(0))
+    expect_equal(sol_free$freeParams, "kc0")
 })
 
-test_that("2-CMT model can be solved analytically", {
-
+test_that("2-CMT analytical solution matches numerical ODE solution", {
     paramValues <- list(kc0 = 1, kcp = 2, kpc = 3)
+    times <- 0:3
 
-    # Testing 2-CMT model via ODE or analytical solution
-    M <- multiCompModel(ncomp = 2, type = "micro") |>
+    M <- compartment_model() |>
+        add_compartment(c("cen", "per"), volume = NA_real_) |>
+        add_molecule(
+            "drug",
+            cmt = c("cen", "per"),
+            initial = c(10, 0),
+            type = "amount"
+        ) |>
+        add_transport("cen", "", const = "kc0", molec = "drug") |>
+        add_transport("cen", "per", const = "kcp", molec = "drug") |>
+        add_transport("per", "cen", const = "kpc", molec = "drug") |>
         add_parameter(param = do.call(parameters, paramValues))
-    M$compartments$initial[[1]] <- 10
 
     sol_aly <- to_analytical(M)
-    y_aly <- sol_aly$statefun(t = 0:3, params = paramValues)
+    y_aly <- sol_aly$statefun(times, params = paramValues)
 
     sol_num <- to_ode(M)
-    y_num <- deSolve::ode(y=sol_num$y0, times = 0:3, func = sol_num$odefun)
+    y_num <- deSolve::ode(
+        y = sol_num$y0,
+        times = times,
+        func = sol_num$odefun,
+        parms = list()
+    )
     attributes(y_num) <- attributes(y_num)[c("dim", "dimnames")]
 
-    expect_equal(y_aly,y_num, tolerance = 1e-4)
-
+    expect_equal(y_aly, y_num, tolerance = 1e-4)
 })
 
-test_that("1-CMT model observables are correctly handled", {
+test_that("1-CMT analytical observables follow ODE observable contract", {
+    params <- list(kc0 = 1, Vcen = 2)
+    times <- 0:3
 
-    params <- list(kc0 = 1, Vc = 2)
-
-    M <- multiCompModel(ncomp = 1, type = "micro") |>
+    M <- compartment_model() |>
+        add_compartment("cen", volume = "Vcen") |>
+        add_molecule("drug", cmt = "cen", initial = 10, type = "amount") |>
+        add_transport("cen", "", const = "kc0", molec = "drug") |>
+        add_observable(Ccen = c[drug, cen]) |>
         add_parameter(param = do.call(parameters, params))
-    M$compartments$initial[[1]] <- 10
 
-    sol_aly <- to_analytical(M, paramValues = params)
+    sol_aly <- to_analytical(M)
     sol_num <- to_ode(M)
 
-    expect_identical(
-        functionBody(sol_num$obsFuncs$Ccen),
-        functionBody(sol_aly$obsFuncs$Ccen)
+    y_aly <- sol_aly$statefun(times)
+    y_num <- deSolve::ode(
+        y = sol_num$y0,
+        times = times,
+        func = sol_num$odefun,
+        parms = list()
     )
 
+    expect_identical(
+        functionBody(sol_aly$obsFuncs$Ccen),
+        functionBody(sol_num$obsFuncs$Ccen)
+    )
+    expect_equal(
+        sol_aly$obsFuncs$Ccen(c(0, 2), y_aly, list()),
+        c(5, 5 * exp(-2))
+    )
+    expect_equal(
+        sol_num$obsFuncs$Ccen(c(0, 2), y_num, list()),
+        c(5, 5 * exp(-2)),
+        tolerance = 1e-6
+    )
 })
